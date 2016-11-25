5b374b6fff6baeeb3253310a6abbbf5209b69708
Andre Mendes <patriqdesigns@gmail.com>
2016-11-25 14:01:52 +0000

    Final formatted schema.

--- schema.sql
+++ schema.sql
@@ -1,116 +1,121 @@
-CREATE TABLE User
-  ( 
-     nif      INTEGER, 
-     nome     CHAR(25), 
-     telefone INTEGER, 
-     CONSTRAINT userKey PRIMARY KEY (nif) 
-  ) ;
+DROP DATABASE IF EXISTS proj;
+CREATE DATABASE proj;
+USE proj;
 
-CREATE TABLE Fiscal 
-  ( 
-     id      INTEGER, 
-     empresa CHAR(25), 
-     CONSTRAINT fiscalKey PRIMARY KEY (id) 
-  ) ;
+CREATE TABLE reserva
+(
+  numero INTEGER,
+  PRIMARY KEY (numero)
+);
 
-CREATE TABLE Edificio 
-  ( 
-     morada CHAR(50), 
-     CONSTRAINT edificioKey PRIMARY KEY (morada) 
-  ) ;
 
-CREATE TABLE Alugavel 
-  ( 
-     morada CHAR(50), 
-     codigo INTEGER, 
-     foto   LONGBLOB, 
-     CONSTRAINT alugavelKey PRIMARY KEY (morada, codigo), 
-     FOREIGN KEY (morada) REFERENCES Edificio(morada) 
-  ) ;
+CREATE TABLE user
+(
+  nif      INTEGER,
+  nome     CHAR(25),
+  telefone INTEGER,
+  PRIMARY KEY (nif)
+);
 
-CREATE TABLE Arrenda 
-  ( 
-     morada  CHAR(50), 
-     codigo INTEGER, 
-     nif     INTEGER, 
-     CONSTRAINT arrendaKey PRIMARY KEY (morada, codigo), 
-     FOREIGN KEY (morada, codigo) REFERENCES Alugavel(morada, codigo), 
-     FOREIGN KEY (nif) REFERENCES User(nif) 
-  ) ;
+CREATE TABLE fiscal
+(
+  id      INTEGER,
+  empresa CHAR(25),
+  PRIMARY KEY (id)
+);
 
-CREATE TABLE Fiscaliza 
-  ( 
-     id     INTEGER, 
-     morada CHAR(50), 
-     codigo INTEGER, 
-     CONSTRAINT fiscalizaKey PRIMARY KEY (id, morada, codigo), 
-     FOREIGN KEY (id) REFERENCES Fiscal(id) 
-     FOREIGN KEY (morada, codigo) REFERENCES Arrenda(morada, codigo) 
-  ) ;
+CREATE TABLE edificio
+(
+  morada CHAR(50),
+  PRIMARY KEY (morada)
+);
 
-CREATE TABLE Espaco 
-  ( 
-     morada CHAR(50), 
-     codigo INTEGER, 
-     CONSTRAINT espacoKey PRIMARY KEY (morada, codigo), 
-     FOREIGN KEY (morada, codigo) REFERENCES Alugavel(morada, codigo) 
-  ) ;
+CREATE TABLE alugavel
+(
+  morada CHAR(50),
+  codigo INTEGER,
+  foto   LONGBLOB,
+  PRIMARY KEY (morada, codigo),
+  FOREIGN KEY (morada) REFERENCES edificio (morada)
+);
 
-CREATE TABLE Posto 
-  ( 
-     morada        CHAR(50), 
-     codigo        INTEGER, 
-     codigo_espaco INTEGER, 
-     CONSTRAINT postoKey PRIMARY KEY (morada, codigo), 
-     FOREIGN KEY (morada, codigo) REFERENCES Alugavel(morada, codigo) 
-     FOREIGN KEY (morada, codigo) REFERENCES Espaco(morada, codigo) 
-  ) ;
+CREATE TABLE arrenda
+(
+  morada CHAR(50),
+  codigo INTEGER,
+  nif    INTEGER,
+  PRIMARY KEY (morada, codigo),
+  FOREIGN KEY (morada, codigo) REFERENCES alugavel (morada, codigo),
+  FOREIGN KEY (nif) REFERENCES user (nif)
+);
 
-CREATE TABLE Oferta 
-  ( 
-     morada      CHAR(50), 
-     codigo      INTEGER, 
-     data_inicio DATE, 
-     data_fim    DATE, 
-     tarifa      NUMERIC(8, 2), 
-     CONSTRAINT ofertaKey PRIMARY KEY (morada, codigo, data_inicio), 
-     FOREIGN KEY (morada, codigo) REFERENCES Alugavel(morada, codigo) 
-  ) ;
+CREATE TABLE fiscaliza
+(
+  id     INTEGER,
+  morada CHAR(50),
+  codigo INTEGER,
+  PRIMARY KEY (id, morada, codigo),
+  FOREIGN KEY (id) REFERENCES fiscal (id),
+  FOREIGN KEY (morada, codigo) REFERENCES arrenda (morada, codigo)
+);
 
-CREATE TABLE Aluga 
-  ( 
-     morada      CHAR(50), 
-     codigo      INTEGER, 
-     data_inicio DATE, 
-     nif         INTEGER, 
-     numero      INTEGER, 
-     CONSTRAINT alugaKey PRIMARY KEY(morada, codigo, data_inicio, nif, numero), 
-     FOREIGN KEY (morada, codigo, data_inicio) REFERENCES Oferta(morada, codigo, 
-     data_inicio) 
-     FOREIGN KEY (nif) REFERENCES User(nif), 
-     FOREIGN KEY (numero) REFERENCES Reserva(numero) 
-  ) ;
+CREATE TABLE espaco
+(
+  morada CHAR(50),
+  codigo INTEGER,
+  PRIMARY KEY (morada, codigo),
+  FOREIGN KEY (morada, codigo) REFERENCES alugavel (morada, codigo)
+);
 
-CREATE TABLE Paga 
-  ( 
-     numero INTEGER, 
-     data   DATE, 
-     metodo CHAR(50), 
-     CONSTRAINT pagaKey PRIMARY KEY (numero), 
-     FOREIGN KEY (numero) REFERENCES Reserva (numero) 
-  ) ;
+CREATE TABLE posto
+(
+  morada        CHAR(50),
+  codigo        INTEGER,
+  codigo_espaco INTEGER,
+  PRIMARY KEY (morada, codigo),
+  FOREIGN KEY (morada, codigo) REFERENCES alugavel (morada, codigo),
+  FOREIGN KEY (morada, codigo_espaco) REFERENCES espaco (morada, codigo)
+);
 
-CREATE TABLE Estado 
-  ( 
-     numero    INTEGER, 
-     timestamp TIMESTAMP, 
-     estado    CHAR(50), 
-     CONSTRAINT estadoKey PRIMARY KEY (numero, timestamp), 
-     FOREIGN KEY(numero) REFERENCES Reserva(numero) 
-  ) ;
+CREATE TABLE oferta
+(
+  morada      CHAR(50),
+  codigo      INTEGER,
+  data_inicio DATE,
+  data_fim    DATE,
+  tarifa      NUMERIC(8, 2),
+  PRIMARY KEY (morada, codigo, data_inicio),
+  FOREIGN KEY (morada, codigo) REFERENCES alugavel (morada, codigo)
+);
 
-CREATE TABLE Reserva 
-  ( 
-     numero INTEGER, 
-     CONSTRAINT reservaKey PRIMARY KEY(numero) 
-  ) ;
+CREATE TABLE aluga
+(
+  morada      CHAR(50),
+  codigo      INTEGER,
+  data_inicio DATE,
+  nif         INTEGER,
+  numero      INTEGER,
+  PRIMARY KEY (morada, codigo, data_inicio, nif, numero),
+  FOREIGN KEY (morada, codigo, data_inicio) REFERENCES oferta (morada, codigo, data_inicio),
+  FOREIGN KEY (nif) REFERENCES user (nif),
+  FOREIGN KEY (numero) REFERENCES reserva (numero)
+);
+
+CREATE TABLE paga
+(
+  numero INTEGER,
+  data   DATE,
+  metodo CHAR(10),
+  PRIMARY KEY (numero),
+  FOREIGN KEY (numero) REFERENCES reserva (numero)
+);
+
+CREATE TABLE estado
+(
+  numero    INTEGER,
+  timestamp TIMESTAMP,
+  estado    CHAR(10),
+  PRIMARY KEY (numero, timestamp),
+  FOREIGN KEY (numero) REFERENCES reserva (numero)
+);
+
