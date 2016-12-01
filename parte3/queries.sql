43bdafa7ef7cc2c7345d8eed759f07eb9eaf4649
Nuno Anselmo <n.anselmo@hotmail.com>
2016-12-01 23:42:01 +0000

    Finished queries

--- parte3/queries.sql
+++ parte3/queries.sql
@@ -1,4 +1,4 @@
-a) Quais os espaços com postos que nunca foram alugados?
+-- a) Quais os espaços com postos que nunca foram alugados?
 
 SELECT DISTINCT p.morada,
                 p.codigo_espaco
@@ -8,18 +8,16 @@ FROM   posto p
                        AND p.codigo = a.codigo
 WHERE  a.numero IS NULL;
 
-b) Quais edifícios com um número de reservas superior à média?
-
-Calcular média alugueres/edificios
-Selecionar onde numero de alugueres superior à média
+-- b) Quais edifícios com um número de reservas superior à média?
 
 SELECT morada
 FROM   aluga
 GROUP  BY morada
-HAVING (SELECT Count(*)
-        FROM   aluga) >= ( Count(*) / Count(DISTINCT morada) );
+HAVING Count(*) > ( (SELECT Count(*)
+                     FROM   aluga) / (SELECT Count(*)
+                                      FROM   edificio) );
 
-c) Quais utilizadores cujos alugáveis foram fiscalizados sempre pelo mesmo fiscal?
+-- c) Quais utilizadores cujos alugáveis foram fiscalizados sempre pelo mesmo fiscal?
 
 SELECT nif
 FROM   fiscaliza
@@ -27,48 +25,104 @@ FROM   fiscaliza
 GROUP  BY nif
 HAVING Count(DISTINCT id) = 1;
 
-d) Qual o montante total realizado (pago) por cada espaço durante o ano de 2016? Assuma que a tarifa indicada na oferta é diária. Deve considerar os casos em que o espaço foi alugado totalmente ou por postos.
+-- d) Qual o montante total realizado (pago) por cada espaço durante o ano de 2016? Assuma que a tarifa indicada na oferta é diária. Deve considerar os casos em que o espaço foi alugado totalmente ou por postos.
 
-SELECT e.morada,
-       e.codigo,
-       Sum(o.tarifa) * 365 AS montanteAnual
-FROM   paga p
-       NATURAL JOIN aluga a
-       NATURAL JOIN oferta o
-       NATURAL JOIN espaco e
-WHERE  p.data BETWEEN '2016-01-01' AND '2016-12-31'
-GROUP  BY e.morada,
-          e.codigo;
+SELECT morada,
+       codigo,
+       Sum(montante)
+FROM   ((SELECT morada,
+                codigo_espaco                            AS codigo,
+                Datediff(data_fim, data_inicio) * tarifa AS montante
+         FROM   aluga
+                NATURAL JOIN oferta
+                NATURAL JOIN posto
+                NATURAL JOIN estado
+         WHERE  estado = 'Paga'
+                AND Year(time_stamp) = 2016)
+        UNION
+        (SELECT morada,
+                codigo,
+                Datediff(data_fim, data_inicio) * tarifa AS montante
+         FROM   aluga
+                NATURAL JOIN oferta
+                NATURAL JOIN espaco
+                NATURAL JOIN estado
+         WHERE  estado = 'Paga'
+                AND Year(time_stamp) = 2016)) t
+GROUP  BY morada,
+          codigo;
 
-e) Quais os espaços de trabalho cujos postos nele contidos foram todos alugados? (Por alugado entende-se um posto de trabalho que tenha pelo menos uma oferta aceite, independentemente das suas datas.)
+-- e) Quais os espaços de trabalho cujos postos nele contidos foram todos alugados? (Por alugado entende-se um posto de trabalho que tenha pelo menos uma oferta aceite, independentemente das suas datas.)
 
-SELECT codigo_espaco
-FROM   aluga
-       NATURAL JOIN estado
-       NATURAL JOIN posto
-GROUP  BY codigo_espaco
-HAVING (SELECT Count(codigo) AS numPostos
+SELECT morada,
+       codigo_espaco
+FROM   (SELECT morada,
+               codigo_espaco,
+               Count(*) AS count
         FROM   posto
-        GROUP  BY codigo_espaco) = (SELECT Count(morada)
-                                    FROM   aluga
-                                           NATURAL JOIN estado
-                                    WHERE  estado = 'aceite');
+        GROUP  BY morada,
+                  codigo_espaco) r1
+       NATURAL JOIN (SELECT morada,
+                            codigo_espaco,
+                            Count(*) AS count
+                     FROM   (SELECT morada,
+                                    codigo_espaco
+                             FROM   posto
+                                    NATURAL JOIN aluga
+                                    NATURAL JOIN estado
+                             WHERE  estado = 'Aceite') p
+                     GROUP  BY morada,
+                               codigo_espaco) r2;
 
-z) Quais as ofertas que não têm reservas associadas cujos estados sejam Paga ou Aceite
+-- y) Qual o montante total realizado (pago) por cada espaço do utilizador de nif '143856248'?
 
-SELECT o.morada, o.codigo, o.data_inicio, o.data_fim, o.tarifa
-FROM oferta o LEFT OUTER JOIN (
-  SELECT morada, codigo
-  FROM aluga NATURAL JOIN (
-    SELECT numero
-    FROM estado e NATURAL JOIN (
-      SELECT numero, MAX(time_stamp) AS time_stamp
-      FROM estado
-      GROUP BY numero
-    ) f
-    WHERE estado = "Aceite" OR estado = "Paga"
-  ) z
-) s
-ON o.morada = s.morada
-AND o.codigo = s.codigo
-WHERE s.codigo IS NULL;
+SELECT morada,
+       codigo,
+       Sum(montante)
+FROM   ((SELECT morada,
+                codigo_espaco                            AS codigo,
+                Datediff(data_fim, data_inicio) * tarifa AS montante
+         FROM   aluga
+                NATURAL JOIN oferta
+                NATURAL JOIN posto
+                NATURAL JOIN (SELECT morada,
+                                     codigo
+                              FROM   arrenda
+                              WHERE  nif = '113056729') u1)
+        UNION
+        (SELECT morada,
+                codigo,
+                Datediff(data_fim, data_inicio) * tarifa AS montante
+         FROM   aluga
+                NATURAL JOIN oferta
+                NATURAL JOIN espaco
+                NATURAL JOIN (SELECT morada,
+                                     codigo
+                              FROM   arrenda
+                              WHERE  nif = '113056729') u2)) t
+GROUP  BY morada,
+          codigo;
+
+-- z) Quais as ofertas que não têm reservas associadas cujos estados sejam Paga ou Aceite?
+
+SELECT o.morada,
+       o.codigo,
+       o.data_inicio,
+       o.data_fim,
+       o.tarifa
+FROM   oferta o
+       LEFT OUTER JOIN (SELECT morada,
+                               codigo
+                        FROM   aluga
+                               NATURAL JOIN (SELECT numero
+                                             FROM   estado e
+                                                    NATURAL JOIN (SELECT
+                               numero,
+                               Max(time_stamp) AS time_stamp
+                                             FROM   estado
+                                             GROUP  BY numero) f
+                                             WHERE  estado = "aceite"
+                                                     OR estado = "paga") z) s
+                    ON o.morada = s.morada
+                       AND o.codigo = s.codigo
+WHERE  s.codigo IS NULL; 
