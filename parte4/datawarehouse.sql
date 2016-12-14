bed2400949cd498df819d2e17267cbd54d2fa712
Andre Mendes <patriqdesigns@gmail.com>
2016-12-14 09:02:01 +0000

    Fixed data warehouse schema.

--- parte4/datawarehouse.sql
+++ parte4/datawarehouse.sql
@@ -1,42 +1,56 @@
-CREATE TABLE user(
-  nif INTEGER,
-  nome CHAR(20),
+USE proj_dw;
+
+SET FOREIGN_KEY_CHECKS = 0;
+
+DROP TABLE IF EXISTS user;
+CREATE TABLE user (
+  nif      INTEGER,
+  nome     CHAR(80),
   telefone INTEGER,
-  PRIMARY KEY (nif));
+  PRIMARY KEY (nif)
+);
 
+DROP TABLE IF EXISTS date_dimension;
 CREATE TABLE date_dimension (
-  date_id INTEGER,
-  dia integer,
-  semana integer,
-  mes integer,
-  semestre integer,
-  ano integer,
-  PRIMARY KEY(date_id) );
-
-CREATE TABLE local_dimension(
-  local_id INTEGER,
-  cod_espaco VARCHAR(50),
-  cod_posto VARCHAR(50),
-  cod_edificio VARCHAR(50),
-  PRIMARY KEY(local_id)
+  date_id  INTEGER,
+  dia      INTEGER,
+  semana   INTEGER,
+  mes      INTEGER,
+  semestre INTEGER,
+  ano      INTEGER,
+  PRIMARY KEY (date_id)
 );
 
-CREATE TABLE time_dimension(
-  time_id INTEGER,
-  minuto_do_dia INTEGER
-    PRIMARY KEY(time_id)
+DROP TABLE IF EXISTS local_dimension;
+CREATE TABLE local_dimension (
+  local_id     VARCHAR(765),
+  cod_espaco   VARCHAR(255),
+  cod_posto    VARCHAR(255),
+  cod_edificio VARCHAR(255),
+  PRIMARY KEY (local_id)
 );
 
-CREATE TABLE reserva(
-  nif integer,
-  date_id INTEGER,
+DROP TABLE IF EXISTS time_dimension;
+CREATE TABLE time_dimension (
   time_id INTEGER,
-  local_id INTEGER,
-  total_pago,
+  hora    INTEGER,
+  minuto  INTEGER,
+  PRIMARY KEY (time_id)
+);
+
+DROP TABLE IF EXISTS reserva;
+CREATE TABLE reserva (
+  nif             INTEGER,
+  date_id         INTEGER,
+  time_id         INTEGER,
+  local_id        VARCHAR(765),
+  total_pago      FLOAT,
   duracao_em_dias INTEGER,
-  PRIMARY KEY (nif, date_id,time_id,local_id),
-  FOREIGN KEY (date_id) REFERENCES date_dimension(date_id),
-  FOREIGN KEY (time_id) REFERENCES time_dimension(time_id),
-  FOREIGN KEY(nif) REFERENCES user(nif),
-  FOREIGN KEY(local_id) REFERENCES local_dimension(local_id)
+  PRIMARY KEY (nif, date_id, time_id, local_id),
+  FOREIGN KEY (date_id) REFERENCES date_dimension (date_id),
+  FOREIGN KEY (time_id) REFERENCES time_dimension (time_id),
+  FOREIGN KEY (nif) REFERENCES user (nif),
+  FOREIGN KEY (local_id) REFERENCES local_dimension (local_id)
 );
+
+SET FOREIGN_KEY_CHECKS = 1;
